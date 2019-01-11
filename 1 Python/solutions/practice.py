54e66264d07b53f4219d8c6304a1f519ef273533
DED8IRD <ewu72829@gmail.com>
2019-01-11 11:05:22 -0800

    dictionary example

--- 1 Python/solutions/practice.py
+++ 1 Python/solutions/practice.py
@@ -76,9 +76,3 @@ def combine_lists_to_dict(keys, values):
     {'a': 'apples', 'b': 'bananas', 'c': 'cherries'}
     """
     return dict(zip(keys, values))
-
-
-socks = ['ankle', 'knee']
-counts = [1, 9000]
-sock_count = combine_lists_to_dict(socks, counts)
-print(sock_count)
